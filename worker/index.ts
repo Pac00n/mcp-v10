f1c2cf11d3504eddc8e197a5c70f4e54b06c2477
Craig Dennis <craigsdennis@gmail.com>
2025-05-12 17:38:06 -0700

    Adds types

--- worker/index.ts
+++ worker/index.ts
@@ -3,15 +3,21 @@ import OpenAi from 'openai';
 
 const app = new Hono<{ Bindings: Env }>();
 
+type ChatInterface = {
+  userMessage: string;
+  previousResponseId?: string;
+}
+
 app.post('/api/chat', async(c) => {
-  const payload = await c.req.json();
+  const payload = await c.req.json<ChatInterface>();
     const client = new OpenAi({
       apiKey: c.env.OPENAI_API_KEY
     });
+    // Stores by default
     const response = await client.responses.create({
-      input: [{role: "user", content: payload.userMessage as string}],
+      input: [{role: "user", content: payload.userMessage}],
       model: "gpt-4.1",
-      previous_response_id: payload.previousResponseId ?? undefined
+      previous_response_id: payload.previousResponseId
     });
     return c.json(response);
 });
