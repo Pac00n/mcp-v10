9feea23a0d5b2f8a37ad13ab7b82efb429db781e
Craig Dennis <craigsdennis@gmail.com>
2025-05-12 17:34:47 -0700

    Adds API for responses with previousResponseId support

--- worker/index.ts
+++ worker/index.ts
@@ -1,12 +1,19 @@
-export default {
-  fetch(request) {
-    const url = new URL(request.url);
+import { Hono } from 'hono';
+import OpenAi from 'openai';
 
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
-    }
-		return new Response(null, { status: 404 });
-  },
-} satisfies ExportedHandler<Env>;
+const app = new Hono<{ Bindings: Env }>();
+
+app.post('/api/chat', async(c) => {
+  const payload = await c.req.json();
+    const client = new OpenAi({
+      apiKey: c.env.OPENAI_API_KEY
+    });
+    const response = await client.responses.create({
+      input: [{role: "user", content: payload.userMessage as string}],
+      model: "gpt-4.1",
+      previous_response_id: payload.previousResponseId ?? undefined
+    });
+    return c.json(response);
+});
+
+export default app;
