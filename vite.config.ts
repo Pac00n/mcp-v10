44c492c8527a1578df297d68151f6d9956df07ed
Craig Dennis <craigsdennis@gmail.com>
2025-05-12 17:20:48 -0700

    Adds shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
-
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(), cloudflare()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
